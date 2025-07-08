d164d6d0d6157120f34c857195079bfe864fa5a7
Nyima-ui <ntenzin492@gmail.com>
2025-07-08 21:11:41 +0530

    made nav responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,41 +5,29 @@ import avatar from "../../public/avatar.svg";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center h-[56px] border-b w-full border-white px-[31px] justify-between">
+    <div className="flex items-center h-[56px] border-b w-full border-white px-3 sm:px-8 justify-between gap-5 sm:gap-20">
       <Image
         src={bars}
+        height={39}
+        width={39}
         alt="Menu button"
-        width={40}
-        height={40}
         className="cursor-pointer"
       />
-      <div className="flex items-center bg-white/50 w-[40%] rounded-[5px] py-1.5 px-2 gap-1.5 text-white focus-within:bg-white focus-within:text-primary">
-        <svg
-          width="23"
-          height="23"
-          viewBox="0 0 23 23"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M15.1374 15.1523L20.125 20.125M17.25 10.0625C17.25 14.032 14.032 17.25 10.0625 17.25C6.09295 17.25 2.875 14.032 2.875 10.0625C2.875 6.09295 6.09295 2.875 10.0625 2.875C14.032 2.875 17.25 6.09295 17.25 10.0625Z"
-            stroke="currentColor"
-            stroke-width="1.67"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+      <div className="flex bg-white/52  p-1.5 rounded-[5px] gap-0.5 flex-grow max-w-[538px]">
+        <Image src={search} height={25} width={25} alt="Seach button" />
         <input
           type="text"
-          className="placeholder-gray-900 text-base text-primary py-1 flex-grow px-2 focus:outline-none"
+          className="inline-block w-full focus:outline-none  text-primary"
           placeholder="Search for a note"
+          name="Search"
+          id="Search"
         />
       </div>
       <Image
         src={avatar}
-        alt="Menu button"
-        width={40}
-        height={40}
+        height={37}
+        width={37}
+        alt="Avatar"
         className="cursor-pointer"
       />
     </div>
