58f5cfa6f53cbdeb7e463dae1f89239b283b44e9
Nyima-ui <ntenzin492@gmail.com>
2025-07-09 20:39:07 +0530

    Remove node_modules from Git tracking

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from 'express'; 
-import cors from 'cors'
 import dotenv from 'dotenv';
+import cors from 'cors'
 
 dotenv.config(); 
 
 const app = express(); 
+const PORT = process.env.PORT || 5000; 
+
+app.use(cors()); 
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+    res.send("Hello world, we got the. fiqr not"); 
+})
+
+app.listen(PORT, () => {
+    console.log(`Server up and running at PORT ${PORT}`); 
+})
 
 
 
