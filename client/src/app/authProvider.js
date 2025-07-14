3b7120ab898fadebc71f78906d7f00697e935439
Tenzin Nyima <ntenzin492@gmail.com>
2025-07-14 13:49:41 +0530

    Update authProvider.js

--- client/src/app/authProvider.js
+++ client/src/app/authProvider.js
@@ -80,7 +80,7 @@ const AuthProvider = ({ children }) => {
 
   const api = useMemo(() => {
     const instance = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: "https://note-taking-backend-llit.onrender.com",
     });
     if (session?.access_token) {
       instance.defaults.headers.common[
